0fe51c91f1095d6f32d2187f1d3649ebce9f4c98
Jeff Heidel <jeh295@cornell.edu>
2012-12-30 22:34:39 -0800

    Time announcements require partial wake lock

--- src/com/jeffheidel/skiapp/TimeAnnounceService.java
+++ src/com/jeffheidel/skiapp/TimeAnnounceService.java
@@ -9,9 +9,11 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.IBinder;
+import android.os.PowerManager;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.TextToSpeech.OnInitListener;
 import android.util.Log;
@@ -79,6 +81,9 @@ public class TimeAnnounceService extends Service implements OnInitListener {
 
 	private NotificationManager nm;
 
+	private PowerManager pm;
+	private PowerManager.WakeLock wl;
+	
 	@Override
 	public void onCreate() {
 		Log.d(TAG, "onCreate");
@@ -88,6 +93,10 @@ public class TimeAnnounceService extends Service implements OnInitListener {
 		Toast.makeText(this, "Time Announcements Started", Toast.LENGTH_SHORT)
 				.show();
 		showNotification();
+		
+		pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+		wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
+		wl.acquire();
 	}
 
 	@Override
@@ -97,6 +106,7 @@ public class TimeAnnounceService extends Service implements OnInitListener {
 		mHandler.removeCallbacks(periodicTask);
 		Toast.makeText(this, "Time Announcements Stopped", Toast.LENGTH_SHORT)
 				.show();
+		wl.release();
 		Log.d(TAG, "onDestroy");
 	}
 
